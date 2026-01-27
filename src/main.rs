2381ad87d91e04bb99d6fac18d8dabf90a88814c
Arseniy <arseny.sergeenko@gmail.com>
2026-01-27 19:46:01 +0200

    added parsing logic

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use crate::{card::SCCard, panels::OptionPanel};
 #[derive(Default)]
 struct ProxyTool<'a> {
     cards: Vec<SCCard>,
+    card_input: String,
     details: DetailPanel<'a>,
     options: OptionPanel,
 }
@@ -27,6 +28,44 @@ fn main() -> iced::Result {
 }
 
 impl ProxyTool<'_> {
+    fn parse_card_list(&mut self) {
+        self.cards.clear();
+        for line in self.card_input.lines() {
+            let line = line.trim();
+
+            // skip empty lines and comments:
+            if line.is_empty() || line.starts_with("#") || line.starts_with("//") {
+                continue
+            }
+
+            let (quantity, name) = if let Some(first_char) = line.chars().next() {
+                if first_char.is_ascii_digit() {
+                    let mut split_idx = 0;
+
+                    for (i, c) in line.char_indices() {
+                        if !c.is_ascii_digit() {
+                            split_idx = i;
+                            break;
+                        }
+                    }
+                    let qty_str = &line[..split_idx];
+                    let mut rest = line[split_idx..].trim_start();
+
+                    if rest.starts_with("x") || rest.starts_with("X") {
+                        rest = rest[1..].trim_start();
+                    }
+
+                    let qty: u32 = qty_str.parse().unwrap_or(1);
+                    (qty, rest.to_string())
+                } else {
+                    (1, line.to_string())
+                }
+            } else {
+                continue;
+            };
+        }
+    }
+
     fn update(&mut self, message: Message) -> iced::Task<Message> {
         match message {
             Message::Random => Task::perform(SCCard::random(), Message::CardFound),
